ea9dd8e1a7a01f120fc62d889ecc9337aa2fb46f
Arthur Bellec <arthur.bellec@gmail.com>
2025-02-20 15:27:58 +0100

    create a function that adds an activity

--- calculatrice_budget.js
+++ calculatrice_budget.js
@@ -4,3 +4,21 @@ let destination = {
     "activities": []
 }
 
+const addActivity = (destination, activityName, cost) => {
+    destination.activities.push({
+        "name": activityName,
+        "cost": cost
+    });
+
+}
+
+
+addActivity(destination, "Visite du Louvre", 20);
+console.log(destination.activities);
+
+// [{ name: "Visite du Louvre", cost: 20 }]
+
+addActivity(destination, "Tour Eiffel", 50);
+console.log(destination.activities);
+
+console.log(destination);
