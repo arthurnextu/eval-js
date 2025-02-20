5233283fe9456409e2dbd51d069902e60f0e1a22
Arthur Bellec <arthur.bellec@gmail.com>
2025-02-20 16:49:17 +0100

    create a function that shows a description of the trip

--- calculatrice_budget.js
+++ calculatrice_budget.js
@@ -1,49 +1,43 @@
-// création d'un objet
 let destination = {
-    "city": "Paris",
-    "country": "France",
-    "activities": []
+    city: 'Paris',
+    country: 'France',
+    activities: []
 }
 
-// Ajout d'une activité
-const addActivity = (destination, activityName, cost) => {
-    destination.activities.push({
-        "name": activityName,
-        "cost": cost
-    });
-
+function addActivity(destination, activityName, cost) {
+    destination.activities.push({activityName, cost})
 }
 
+addActivity(destination, 'Louvre', 30)
+addActivity(destination, 'Eiffel Tower', 40)
+addActivity(destination, 'Notre Dame', 20)
+console.log(destination);
 
-addActivity(destination, "Visite du Louvre", 20);
-console.log(destination.activities);
-
-// [{ name: "Visite du Louvre", cost: 20 }]
-
-addActivity(destination, "Tour Eiffel", 50);
-console.log(destination.activities)
-
-// Calcul du coût total
-const calculateTotalCost = (destination) => {
-    let total = 0;
+function calculateTotalCost(destination) {
+    let total = 0
     for (let i = 0; i < destination.activities.length; i++) {
-        total += destination.activities[i].cost;
+        total += destination.activities[i].cost
     }
-    return total;
+    return total
 }
 
 console.log(calculateTotalCost(destination));
 
-// tableau de destinations
-const destinations = [];
+let destinations = []
 
-// Ajout d'une destination
-const addDestination = (destinations, destination) => {
-    destinations.push(destination);
+function addDestination(destinations, destination) {
+    destinations.push(destination)
 }
 
-addDestination(destinations, { name: "Tokyo", country: "Japan", activities: []});
-addActivity(destinations[0], "Visite du temple", 30);
-addDestination(destinations, { name: "New York", country: "USA", activities: []});
-addActivity(destinations[1], "Visite de Central Park", 10);
-console.table(destinations);
+addDestination(destinations, destination)
+console.log(destinations);
+
+function displayDestinationSummary(destination) {
+    console.log(`Destination : ${destination.city} (${destination.country}).`)
+    console.log('Activities:');
+    destination.activities.forEach(activity => {
+        console.log(`- ${activity.activityName} (${activity.cost} EUR)`);
+    });
+}
+
+displayDestinationSummary(destination)
